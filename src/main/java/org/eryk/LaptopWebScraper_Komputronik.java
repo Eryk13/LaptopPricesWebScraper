8d33c033868359b0fe4ffbd56cfef4f26dd316aa
Eryk Ciesielski <eryk@192.168.0.104>
2023-09-30 23:57:21 +0200

    add komputronik best deals web scraper

---
+++ src/main/java/org/eryk/LaptopWebScraper_Komputronik.java
@@ -0,0 +1,72 @@
+package org.eryk;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
+public class LaptopWebScraper_Komputronik extends LaptopWebScraper{
+    @Override
+    public List<Laptop> getLaptopDeals() {
+        this.driver.get("https://www.komputronik.pl/category/5022/laptopy.html?plabel%5B%5D=promo-price&filter=1&showBuyActiveOnly=1");
+
+        //accept cookies
+        WebElement el = null;
+        while(el == null) {
+            try {
+                el = driver.findElement(By.id("onetrust-accept-btn-handler"));
+                el.click();
+            }
+            catch(NoSuchElementException ignored) {}
+            catch(Exception ignored) {}
+        }
+
+        //get laptops
+        List<WebElement> elements = new ArrayList<>();
+        ArrayList<Laptop> laptops = new ArrayList<>();
+
+
+        int page =2;
+        boolean run = true;
+        do{
+            try{
+                driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+                elements = driver.findElements(By.cssSelector(".tests-product-entry"));
+                if(elements.isEmpty()){
+                    run = false;
+                }
+                for(WebElement item : elements) {
+                    try{
+                        String title = item.findElement(By.tagName("h2")).getText();
+                        String priceText = item.findElement(By.cssSelector(".text-3xl.font-bold.leading-8")).getText();
+                        priceText = priceText.replace(" ", "").replace("zł", "");
+
+                        int price = Integer.parseInt(priceText);
+
+                        Laptop laptop = new Laptop(title, price);
+                        laptops.add(laptop);
+                    }
+                    catch(Exception ignored){
+                        System.out.println(ignored);
+                    }
+                }
+                elements.clear();
+                driver.get("https://www.komputronik.pl/category/5022/laptopy.html?plabel%5B%5D=promo-price&filter=1&showBuyActiveOnly=1&p="+ page);
+                page++;
+            }
+            catch(Exception e){
+                run = false;
+            }
+        }while(run);
+
+        return laptops;
+    }
+
+    @Override
+    public List<Laptop> getLaptops(String str) {
+        return null;
+    }
+}
--- src/main/java/org/eryk/Main.java
+++ src/main/java/org/eryk/Main.java
@@ -4,14 +4,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        var webScraper = new LaptopWebScraper_RTVEuroAGD();
+        var webScraper = new LaptopWebScraper_Komputronik();
         List<Laptop> laptops = webScraper.getLaptopDeals();
         webScraper.close();
         System.out.println("Laptops: \n");
+        int i = 0;
         for(Laptop item : laptops) {
-            System.out.println("Name: " + item.getName());
-            System.out.println("Price: " + item.getPrice());
+            System.out.println(i + ". Name: " + item.getName());
+            System.out.println(i + ". Price: " + item.getPrice());
             System.out.println();
+            i++;
         }
 
     }
